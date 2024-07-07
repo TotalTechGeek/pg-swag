68a17ffa91b11f0a5b680caf22ea0ba66d73ebb1
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2024-07-07 17:43:12 -0500

    Add logging and "resolves"

--- stress.test.js
+++ stress.test.js
@@ -1,7 +1,7 @@
 import { swag } from './setup.test.js'
 
 /**
- * @test 10000, 30000
+ * @test 10000, 30000 resolves
  */
 export async function Stress (num, under) {
   const now = new Date()
@@ -15,6 +15,7 @@ export async function Stress (num, under) {
       count++
       if (count === num) {
         const time = new Date().getTime() - now.getTime()
+        console.log(`Took ${time}ms to execute ${num} jobs`)
         if (time < under) resolve()
         else reject(new Error(`Took ${time}ms to execute ${num} jobs`))
       }
