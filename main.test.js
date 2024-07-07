4984b0347ba7b9c13ea3cf77ee3827614ea6fee6
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2024-07-07 13:04:47 -0500

    Rewrite to use a scenario test

--- main.test.js
+++ main.test.js
@@ -1,4 +1,7 @@
 import { Swag } from './index.js'
+import { Steps } from 'pineapple'
+
+const { Given, When, Then, Scenario } = Steps()
 
 const swag = new Swag({
   password: 'postgres',
@@ -6,15 +9,23 @@ const swag = new Swag({
   host: 'localhost'
 })
 
-/**
- * @test void resolves
- */
-export async function Example () {
-  await swag.schedule('test', 'Test', 'R1/PT1S', {})
+Given('a topic {topic}', async function (topic) {
+  this.topic = topic
+})
 
-  return new Promise(resolve => {
-    swag.on('test', job => {
-      resolve()
-    })
-  })
-}
+When('I schedule a job with name {name} to run at {expression}', async function (name, expression) {
+  await swag.schedule(this.topic, name, expression, {})
+  this.name = name
+})
+
+Then('I should see the job run', function () {
+  return new Promise(resolve => swag.on(this.topic, resolve))
+})
+
+/**
+ * @test { topic: 'Test', name: 'Joe', expression: 'R1/PT1S' } resolves @.id === 'Joe'
+ */
+export const Once = Scenario`
+Given a topic {topic}
+When I schedule a job with name {name} to run at {expression}
+Then I should see the job run`
