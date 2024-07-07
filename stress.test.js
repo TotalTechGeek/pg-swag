426d8f9d86c1b18c84cfa956a75bf7ef86cb2f9e
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2024-07-07 17:46:08 -0500

    Add another log and stress test

--- stress.test.js
+++ stress.test.js
@@ -2,19 +2,22 @@ import { swag } from './setup.test.js'
 
 /**
  * @test 10000, 30000 resolves
+ * @test 50000, 30000 resolves
  */
 export async function Stress (num, under) {
   const now = new Date()
   for (let i = 0; i < num; i++) {
     await swag.schedule('stress', i, now, {})
   }
+  const after = new Date()
+  console.log(`Took ${after - now}ms to schedule ${num} jobs`)
 
   return new Promise((resolve, reject) => {
     let count = 0
     swag.on('stress', job => {
       count++
       if (count === num) {
-        const time = new Date().getTime() - now.getTime()
+        const time = new Date().getTime() - after.getTime()
         console.log(`Took ${time}ms to execute ${num} jobs`)
         if (time < under) resolve()
         else reject(new Error(`Took ${time}ms to execute ${num} jobs`))
